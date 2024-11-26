94cd04ee14296478df4c7941f5d8d88eb9202eea
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-11-26 23:31:28 +0000

    Add task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+// Create a readline interface to read from stdin (command line input)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Display a welcome message and ask for the user's name
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  // Display the name provided by the user
+  console.log(`Your name is: ${name}`);
+
+  // Close the program and display the closing message
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+  });
+
+  // Close the readline interface
+  rl.close();
+});
