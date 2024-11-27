63bef9c8c99ea8946c023885eabb1fa13e7516d7
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-11-27 00:04:50 +0000

    Add 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,15 +7,11 @@ const rl = readline.createInterface({
 });
 
 // Display a welcome message and ask for the user's name
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+rl.question('Welcome tos Holberton School, what is your name?\n', (name) => {
   // Display the name provided by the user
-  console.log(`Your name is: ${name}`);
+  console.log(`Your names is: ${name}`);
 
-  // Close the readline interface
+  // Close the readline interface and display the closing message
+  console.log('This importaant software is now closing');
   rl.close();
-});
-
-// Display the closing message when the readline interface closes
-rl.on('close', () => {
-  console.log('This important software is now closing');
 });
