7324a93dbaee5adc27723a50b0212f6f0ca14c46
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-11-28 16:48:47 +0000

    Add task 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+    describe('SUM', () => {
+        it('should return 6 when inputs are 1.4 and 4.5', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+    });
+
+    describe('SUBTRACT', () => {
+        it('should return -4 when inputs are 1.4 and 4.5', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+    });
+
+    describe('DIVIDE', () => {
+        it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.closeTo(0.2, 0.001);
+        });
+
+        it('should return "Error" when dividing by 0', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+    });
+
+    describe('Invalid type', () => {
+        it('should throw an error for an invalid type', () => {
+            expect(() => calculateNumber('INVALID', 1.4, 4.5)).to.throw('Invalid type');
+        });
+    });
+});
