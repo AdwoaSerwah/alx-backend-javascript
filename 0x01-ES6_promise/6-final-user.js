40e60234f1c12b80ccdae7ecfc968ffa636707b6
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-11-29 11:27:18 +0000

    Add task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,24 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-    .then((results) => results.map((result) => ({
+  // Use Promise.allSettled to handle both promises
+  const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+
+  // Map through the results and return the correct structure
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value, // for resolved promises
+      };
+    }
+    return {
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+      value: result.reason.toString(), // Return the full error message (Error: ...)
+    };
+  });
 }
