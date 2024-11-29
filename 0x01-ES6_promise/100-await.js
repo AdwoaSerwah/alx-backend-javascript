a7ce1750895fc08084066b7f84d74031fc7d497d
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-11-29 12:28:56 +0000

    Add

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,39 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser() {
-  const result = {
-    photo: null,
-    user: null
-  };
-
+const asyncUploadUser = async () => {
   try {
-    // Wait for both promises to settle
-    const [photoResponse, userResponse] = await Promise.allSettled([
-      uploadPhoto(),
-      createUser()
-    ]);
-
-    // Handle photo response
-    if (photoResponse.status === 'fulfilled') {
-      result.photo = photoResponse.value;
-    } else {
-      result.photo = null;
-    }
-
-    // Handle user response
-    if (userResponse.status === 'fulfilled') {
-      result.user = userResponse.value;
-    } else {
-      result.user = null;
-    }
-
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
   } catch (error) {
-    // Catch any other unexpected errors and return null for both
-    return {
-      photo: null,
-      user: null
-    };
+    return { photo: null, user: null };
   }
+};
 
-  return result;
-}
+export default asyncUploadUser;
