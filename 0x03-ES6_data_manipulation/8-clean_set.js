c096964db9634bc30aa7e91fd3ddee0ec6e5e116
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-22 17:11:39 +0000

    Add 8 again

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,11 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
+  // If startString is empty, return an empty string
+  if (startString === '') {
+    return '';
+  }
+
   set.forEach((value) => {
     if (value.startsWith(startString)) {
       result.push(value.slice(startString.length)); // Remove startString and push the rest
